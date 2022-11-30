ad11902f72a2e84972f811ce8aeec1740cfd82ec
Stjpaa <87935031+Stjpaa@users.noreply.github.com>
2022-11-30 13:49:14 +0100

    install dependencies test

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,7 +1,5 @@
 var Generator = require('yeoman-generator');
 
 module.exports = class extends Generator {
-    testfunc() {
-        this.log("generator works");
-    };
-};
+    installDependencies()
+}
